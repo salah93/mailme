82b68eee908de698337d1b097df6c9fc76ee6144
salah ahmed <salah.ahmed.hslc@gmail.com>
2016-11-30 14:01:25 -0500

    move module to init

---
+++ mailme/__init__.py
@@ -0,0 +1,29 @@
+#! /usr/local/bin/python
+import smtplib
+import sys
+from contextlib import contextmanager 
+
+from credentials import myemail, mypassword
+
+
+def mailto(to, msg):
+    @contextmanager
+    def login(email, password):
+        # port 465 or 587
+        port = 587
+        try:
+            server = smtplib.SMTP('smtp.gmail.com', port) 
+            server.ehlo()
+            server.starttls()
+            server.ehlo()
+            server.login(myemail, mypassword)
+            yield server
+        finally:
+            server.close()
+    with login(myemail, mypassword) as s:
+        s.sendmail(myemail, to, msg)
+
+
+if __name__ == '__main__':
+    to, msg = sys.argv[1:]
+    mailto(to, msg)
